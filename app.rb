0b0dab3b519d7f24902f65bcaad7776e6decdb79
Brian Pfeil <brian.pfeil@gmail.com>
2009-09-24 17:21:32 -0400

    updated to use datamapper

--- app.rb
+++ app.rb
@@ -4,6 +4,36 @@ require 'dm-core'
 require 'appengine-apis/datastore'
 require 'json'
 require 'mime/types'
+require 'base64'
+
+class Sinatra::Reloader < Rack::Reloader
+  def safe_load(file, mtime, stderr = $stderr)
+    ::Sinatra::Application.reset!
+    super
+  end
+end
+
+configure :development do
+    use Sinatra::Reloader
+    use Sinatra::ShowExceptions
+end
+
+DataMapper.setup(:default, "appengine://auto")
+
+class Shout
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :message, Text
+end
+
+class FileItem
+  include DataMapper::Resource
+  
+  property :id, Serial
+  property :path, String
+  property :contents, Text
+end
 
 set :environment, :development
 
@@ -19,13 +49,15 @@ helpers do
 end
 
 get '/' do
-  @files = AppEngine::Datastore::Query.new('File').fetch
+  #@files = AppEngine::Datastore::Query.new('File').fetch
+  @files = FileItem.all
   erb :index
 end
 
 get FS_PATTERN do
   path = params[:splat].first
-  files = AppEngine::Datastore::Query.new('File').filter(:path, :==, path).fetch
+  #files = AppEngine::Datastore::Query.new('File').filter(:path, :==, path).fetch
+  files = FileItem.all(:path => path)
   
   # found a match
   if files.size > 0
@@ -40,7 +72,8 @@ get FS_PATTERN do
       content_type 'text/html'
     end
     
-    file[:contents]
+    #file[:contents]
+    Base64.decode64(file.contents)
   else
     "File at path \"#{path}\" not found"
   end
@@ -48,18 +81,23 @@ get FS_PATTERN do
 end
 
 put FS_PATTERN do
+  redirect '/'
 end
 
 post FS_PATTERN do
   path = params[:splat]
   path = (path.is_a? Array) ? path.first : path
-  file = AppEngine::Datastore::Entity.new('File')
-  file[:path] = path
-  file_contents_blob = AppEngine::Datastore::Blob.new(params[:datafile][:tempfile].read)
-  file.set_property(:contents, file_contents_blob)
-  AppEngine::Datastore.put file
+  #file = AppEngine::Datastore::Entity.new('File')
+  #file[:path] = path
+  #file_contents_blob = AppEngine::Datastore::Blob.new(params[:datafile][:tempfile].read)
+  #file.set_property(:contents, file_contents_blob)
+  #AppEngine::Datastore.put file
+  FileItem.create(:path => path, :contents => Base64.encode64(params[:datafile][:tempfile].read) )
   redirect '/'
 end
 
 delete FS_PATTERN do
+  path = params[:splat].first
+  FileItem.all(:path => path).first.destroy
+  redirect '/'
 end
